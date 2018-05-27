4ce4f479f6bd372dea0262559ade0c72626e7706
rohit <rohit.muneshwar@ymail.com>
2018-05-27 17:08:39 +0530

    Assignments - Mac - till 3D geometry

---
+++ RohitMuneshwarRTRAssignments/OpenGL_On_Mac/Native/4_Mac_Perspective_05052018/window.mm
@@ -0,0 +1,555 @@
+//headers
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+#import <QuartzCore/CVDisplayLink.h>
+
+#import <OpenGL/gl3.h>
+#import <OpenGL/gl3ext.h>
+#import "vmath.h"
+
+//'C' style global function declaration
+CVReturn MyDisplayLinkCallback(CVDisplayLinkRef, const CVTimeStamp *,const CVTimeStamp *,CVOptionFlags,CVOptionFlags *,void *);
+
+//global variables
+FILE *gpFile=NULL;
+
+enum
+{
+ VDG_ATTRIBUTE_VERTEX=0,
+    VDG_ATTRIBUTE_COLOR=1,
+    VDG_ATTRIBUTE_NORMAL=2,
+    VDG_ATTRIBUTE_TEXTURE0=3,
+    
+};
+
+//interface declarations
+@interface AppDelegate : NSObject<NSApplicationDelegate, NSWindowDelegate>
+
+@end
+
+@interface GLView : NSOpenGLView
+
+@end
+
+//entry-point function
+int main(int argc, const char * argv[])
+{
+    //code
+    NSAutoreleasePool *pPool=[[NSAutoreleasePool alloc]init];
+    
+    NSApp=[NSApplication sharedApplication];
+    
+    [NSApp setDelegate:[[AppDelegate alloc]init]];
+    
+    [NSApp run];
+    
+    [pPool release];
+    
+    return(0);
+}
+
+//interface implementation
+@implementation AppDelegate
+{
+    @private
+    NSWindow *window;
+    GLView *glView;
+}
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    //code
+    //log file
+    NSBundle *mainBundle=[NSBundle mainBundle];
+    NSString *appDirName=[mainBundle bundlePath];
+    NSString *parentDirPath=[appDirName stringByDeletingLastPathComponent];
+    NSString *logFileNameWithPath=[NSString stringWithFormat:@"%@/log.txt",parentDirPath];
+    const char *pszLogFileNameWithPath=[logFileNameWithPath cStringUsingEncoding:NSASCIIStringEncoding];
+    gpFile=fopen(pszLogFileNameWithPath,"w");
+    if(gpFile==NULL)
+    {
+        printf("Can not create log file.\nExitting...\n");
+        [self release];
+        [NSApp terminate:self];
+    }
+    fprintf(gpFile,"Program is started successfully...\n");
+    
+    //window
+    NSRect win_rect;
+    win_rect=NSMakeRect(0.0, 0.0, 800.0, 600.0);
+    
+    //create simple window
+    window=[[NSWindow alloc] initWithContentRect:win_rect
+                                       styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
+                                        NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable
+                                         backing:NSBackingStoreBuffered
+                                           defer:NO];
+    
+    [window setTitle:@"macOS OpenGL Window"];
+    [window center];
+    
+    glView=[[GLView alloc]initWithFrame:win_rect];
+    
+    [window setContentView:glView];
+    [window setDelegate:self];
+    [window makeKeyAndOrderFront:self];
+}
+
+- (void)applicationWillTerminate:(NSNotification *)notification
+{
+    //code
+    fprintf(gpFile, "Program is terminated successfully.\n");
+    if(gpFile)
+    {
+        fclose(gpFile);
+        gpFile=NULL;
+    }
+}
+
+- (void)windowWillClose:(NSNotification *)notification
+{
+    //code
+    [NSApp terminate:self];
+}
+
+- (void)dealloc
+{
+    //code
+    [glView release];
+    
+    [window release];
+    
+    [super dealloc];
+}
+@end
+
+@implementation GLView
+{
+    @private
+    CVDisplayLinkRef displayLink;
+    
+    //for shaders
+    GLuint gVertexShaderObject;
+    GLuint gFragmentShaderObject;
+    GLuint gShaderProgramObject;
+    
+    //vertex array and buffer objects
+    GLuint gVao;
+    GLuint gVbo;
+    
+    //uniforms
+    GLuint mvpUniform;
+    
+    //projection matricex
+    vmath::mat4 gPerspectiveProjectionMatrix;
+}
+-(id)initWithFrame:(NSRect)frame;
+{
+    //code
+    self=[super initWithFrame:frame];
+    if(self)
+    {
+        [[self window]setContentView:self];
+        
+        NSOpenGLPixelFormatAttribute attrs[]=
+        {
+            //must specify the 4.1 core profile to use OpenGL 4.1
+            NSOpenGLPFAOpenGLProfile,
+            NSOpenGLProfileVersion4_1Core,
+            //specify the display ID to associate the GL context with (main display for now)
+            NSOpenGLPFAScreenMask,CGDisplayIDToOpenGLDisplayMask(kCGDirectMainDisplay),
+            NSOpenGLPFANoRecovery,
+            NSOpenGLPFAAccelerated,
+            NSOpenGLPFAColorSize, 24,
+            NSOpenGLPFADepthSize, 24,
+            NSOpenGLPFAAlphaSize, 8,
+            NSOpenGLPFADoubleBuffer,
+            0
+        };
+        
+        NSOpenGLPixelFormat *pixelFormat=[[[NSOpenGLPixelFormat alloc]initWithAttributes:attrs]autorelease];
+        
+        if(pixelFormat==nil)
+        {
+            fprintf(gpFile, "No Valid OpenGL Pixel Format Is Available. Exitting...");
+            [self release];
+            [NSApp terminate:self];
+        }
+        
+        NSOpenGLContext *glContext=[[[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil]autorelease];
+        
+        [self setPixelFormat:pixelFormat];
+        
+        [self setOpenGLContext:glContext]; //it automatically releases the older context, if present, and sets the newer one
+    }
+    return(self);
+}
+
+-(CVReturn)getFrameForTime:(const CVTimeStamp *)pOutputTime
+{
+    //code
+    NSAutoreleasePool *pool=[[NSAutoreleasePool alloc]init];
+    
+    [self drawView];
+    
+    [pool release];
+    
+    return(kCVReturnSuccess);
+}
+
+-(void)prepareOpenGL
+{
+    //code
+    //OpenGL Info
+    fprintf(gpFile, "OpenGL Version: %s\n", glGetString(GL_VERSION));
+    fprintf(gpFile, "GLSL Version: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+    
+    [[self openGLContext]makeCurrentContext];
+    
+    GLint swapInt=1;
+    [[self openGLContext]setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
+    
+    //create shader objects
+    //vertex shader
+    gVertexShaderObject = glCreateShader(GL_VERTEX_SHADER);
+    
+    //provide source code to shader
+    const GLchar *vertexShaderSourceCode=
+    "#version 410"\
+    "\n"\
+    "in vec4 vPosition;"\
+    "uniform mat4 u_mvp_matrix;"\
+    "void main(void)"\
+    "{"\
+    "gl_Position=u_mvp_matrix * vPosition;"\
+    "}";
+    
+    //attach shader source to above vertex shader object
+    glShaderSource(gVertexShaderObject, //handle of the shader object whose source code has to be replaced
+                   1, //number of elements in next two arrays
+                   (const GLchar **) &vertexShaderSourceCode, //array of pointers to string containing the source code to be loaded into the shader
+                   NULL //array of string lengths
+                   );
+    
+    //compile shader
+    glCompileShader(gVertexShaderObject);
+    
+    //get compilation status and log the information
+    GLint iInfoLogLength = 0;
+    GLint iShaderCompiledStatus = 0;
+    char* szInfoLog = NULL;
+    
+    //get compilation status
+    glGetShaderiv(gVertexShaderObject, GL_COMPILE_STATUS, &iShaderCompiledStatus);
+    if(iShaderCompiledStatus==GL_FALSE)
+    {
+        //extract log information
+        glGetShaderiv(gVertexShaderObject, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+        if(iInfoLogLength > 0)
+        {
+            szInfoLog = (char *) malloc(iInfoLogLength);
+            if(szInfoLog!=NULL)
+            {
+                GLsizei written;
+                glGetShaderInfoLog(gVertexShaderObject, iInfoLogLength, &written, szInfoLog);
+                fprintf(gpFile, "Vertex Shader Compilation Log : %s\n", szInfoLog);
+                
+                free(szInfoLog);
+                [self release];
+                [NSApp terminate:self];
+            }
+        }
+    }
+    
+    //fragment shader
+    //re-initialize
+    iInfoLogLength = 0;
+    iShaderCompiledStatus = 0;
+    szInfoLog = NULL;
+    
+    //create fragment shader object
+    gFragmentShaderObject = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    //shader source code
+    const GLchar* fragmentShaderSourceCode =
+    "#version 410"\
+    "\n"\
+    "out vec4 FragColor;"\
+    "void main(void)"\
+    "{"\
+    "FragColor = vec4(1.0,1.0,1.0,1.0);"\
+    "}";
+    
+    //attach source code
+    glShaderSource(gFragmentShaderObject, 1, (const GLchar **) &fragmentShaderSourceCode, NULL);
+    
+    //compile shader
+    glCompileShader(gFragmentShaderObject);
+    
+    //get compilation status
+    glGetShaderiv(gFragmentShaderObject, GL_COMPILE_STATUS, &iShaderCompiledStatus);
+    if(iShaderCompiledStatus==GL_FALSE)
+    {
+        //extract log information
+        glGetShaderiv(gFragmentShaderObject, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+        if(iInfoLogLength > 0)
+        {
+            szInfoLog = (char *) malloc(iInfoLogLength);
+            if(szInfoLog!=NULL)
+            {
+                GLsizei written;
+                glGetShaderInfoLog(gFragmentShaderObject, iInfoLogLength, &written, szInfoLog);
+                fprintf(gpFile, "Fragment Shader Compilation Log : %s\n", szInfoLog);
+                
+                free(szInfoLog);
+                [self release];
+                [NSApp terminate:self];
+            }
+        }
+    }
+    
+    //shader program object
+    //create shader program object
+    gShaderProgramObject = glCreateProgram();
+    
+    //attach vertex shader
+    glAttachShader(gShaderProgramObject, gVertexShaderObject);
+    
+    //attach fragment shader
+    glAttachShader(gShaderProgramObject, gFragmentShaderObject);
+    
+    //pre-link binding of shader program object with vertex shader position attribute
+    glBindAttribLocation(gShaderProgramObject, VDG_ATTRIBUTE_VERTEX, "vPosition");
+    
+    //link shader
+    glLinkProgram(gShaderProgramObject);
+    
+    //get link status
+    GLint iShaderProgramLinkStatus = 0;
+    glGetProgramiv(gShaderProgramObject, GL_LINK_STATUS, &iShaderProgramLinkStatus);
+    if(iShaderProgramLinkStatus==GL_FALSE)
+    {
+        //extract log information
+        glGetProgramiv(gShaderProgramObject, GL_INFO_LOG_LENGTH, &iInfoLogLength);
+        if(iInfoLogLength > 0)
+        {
+            szInfoLog = (char *) malloc(iInfoLogLength);
+            if(szInfoLog!=NULL)
+            {
+                GLsizei written;
+                glGetProgramInfoLog(gShaderProgramObject, iInfoLogLength, &written, szInfoLog);
+                fprintf(gpFile,  "Shader Program Link Log : %s\n", szInfoLog);
+                
+                free(szInfoLog);
+                [self release];
+                [NSApp terminate:self];
+            }
+        }
+    }
+    
+    //get uniform locations
+    mvpUniform = glGetUniformLocation(gShaderProgramObject, "u_mvp_matrix");
+    
+    //vertices, colors, shader attribs, vbo, vao initialization
+    const GLfloat triangleVertices[] =
+    {
+      0.0f, 1.0f, 0.0f, //appex
+        -1.0f, -1.0f, 0.0f, //left-bottom
+        1.0f, -1.0f, 0.0f //right-bottom
+    };
+    
+    //configure vao
+    glGenVertexArrays(1, &gVao);
+    glBindVertexArray(gVao);
+    
+    glGenBuffers(1, &gVbo);
+    glBindBuffer(GL_ARRAY_BUFFER, gVbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(VDG_ATTRIBUTE_VERTEX, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    glBindVertexArray(0);
+    
+    //
+    glClearDepth(1.0f);
+    
+    //enable depth testing
+    glEnable(GL_DEPTH_TEST);
+    
+    //depth test to do
+    glDepthFunc(GL_LEQUAL);
+    
+    //we will always cull back faces for better performance
+    glEnable(GL_CULL_FACE);
+    
+    //set background color
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f); //blue
+    
+    gPerspectiveProjectionMatrix = vmath::mat4::identity();
+    
+    //video refresh rate linking
+    CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+    CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
+    CGLContextObj cglContext=(CGLContextObj) [[self openGLContext]CGLContextObj];
+    CGLPixelFormatObj cglPixelFormat = (CGLPixelFormatObj) [[self pixelFormat]CGLPixelFormatObj];
+    CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+    CVDisplayLinkStart(displayLink);
+}
+
+-(void)reshape
+{
+    //code
+    CGLLockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+    
+    NSRect rect=[self bounds];
+    
+    GLfloat width=rect.size.width;
+    GLfloat height=rect.size.height;
+    
+    if(height==0)
+        height=1;
+    
+    glViewport(0,0,(GLsizei)width, (GLsizei)height);
+    
+    //glOrtho(left, right, bottom, top, near, far)
+    /*if(width <= height)
+    {
+        gOrthographicProjectionMatrix = vmath::ortho(-100.0f, 100.0f, (-100.0f * (height/width)), (100.0f * (height/width)), -100.0f, 100.0f);
+    }else{
+        gOrthographicProjectionMatrix = vmath::ortho((-100.0f * (width/height)), (100.0f * (width/height)), -100.0f, 100.0f, -100.0f, 100.0f);
+    }*/
+    gPerspectiveProjectionMatrix = vmath::perspective(45.0f, (GLfloat)width/(GLfloat)height, 0.1f, 100.0f);
+    
+    CGLUnlockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+}
+
+-(void)drawRect:(NSRect)dirtyRect
+{
+    //code
+    [self drawView];
+}
+
+-(void)drawView
+{
+    //code
+    [[self openGLContext]makeCurrentContext];
+    
+    CGLLockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    //start using OpenGL program object
+    glUseProgram(gShaderProgramObject);
+    
+    //OpenGL Drawing
+    //set modelview and projection matrices to identity
+    vmath::mat4 modelViewMatrix = vmath::mat4::identity();
+    vmath::mat4 modelViewProjectionMatrix = vmath::mat4::identity();
+    
+    //translate
+    modelViewMatrix = vmath::translate(0.0f,0.0f,-3.0f);
+    
+    //multiply the modelview and orthographic matrix to get modelViewProjectionMatrix
+    //order is important
+    modelViewProjectionMatrix = gPerspectiveProjectionMatrix * modelViewMatrix;
+    
+    glUniformMatrix4fv(mvpUniform, 1, GL_FALSE, modelViewProjectionMatrix);
+    
+    //bind vao
+    glBindVertexArray(gVao);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    glBindVertexArray(0);
+    
+    glUseProgram(0);
+    CGLFlushDrawable((CGLContextObj)[[self openGLContext]CGLContextObj]);
+    CGLUnlockContext((CGLContextObj)[[self openGLContext]CGLContextObj]);
+}
+
+-(BOOL)acceptsFirstResponder
+{
+    //code
+    [[self window]makeFirstResponder:self];
+    return(YES);
+}
+
+-(void)keyDown:(NSEvent *)theEvent
+{
+    //code
+    int key=(int)[[theEvent characters] characterAtIndex:0];
+    
+    switch(key)
+    {
+        case 27: //Escape
+            [self release];
+            [NSApp terminate:self];
+            break;
+        case 'F':
+        case'f':
+            [[self window]toggleFullScreen:self];
+            break;
+        default:
+            break;
+    }
+}
+
+-(void)mouseDown:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void)mouseDragged:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void)rightMouseDown:(NSEvent *)theEvent
+{
+    //code
+}
+
+-(void) dealloc
+{
+    //code
+    CVDisplayLinkStop(displayLink);
+    
+    //destroy vao
+    if(gVao)
+    {
+        glDeleteVertexArrays(1, &gVao);
+        gVao = 0;
+    }
+    
+    if(gVbo)
+    {
+        glDeleteBuffers(1, &gVbo);
+        gVbo = 0;
+    }
+    
+    //detach vertex shader from shader program object
+    glDetachShader(gShaderProgramObject, gVertexShaderObject);
+    
+    glDetachShader(gShaderProgramObject, gFragmentShaderObject);
+    
+    glDeleteShader(gVertexShaderObject);
+    gVertexShaderObject=0;
+    
+    glDeleteShader(gFragmentShaderObject);
+    gFragmentShaderObject=0;
+    
+    glDeleteProgram(gShaderProgramObject);
+    gShaderProgramObject=0;
+    
+    
+    CVDisplayLinkRelease(displayLink);
+    
+    [super dealloc];
+}
+@end
+
+CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp *pNow, const CVTimeStamp *pOutputTime, CVOptionFlags flagsIn, CVOptionFlags *pFlagsOut, void *pDisplayLinkContext)
+{
+    CVReturn result = [(GLView *)pDisplayLinkContext getFrameForTime:pOutputTime];
+    return(result);
+}
